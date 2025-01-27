b055cbbd9bc00b1039a02d5c25537743b146f5bb
Matheus Almeida <tthheusalmeida@gmail.com>
2025-01-27 10:18:27 -0300

    feat: enable postcss and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   modules: ["@nuxt/ui"],
+  css: ["~/assets/css/styles.css"],
+  postcss: {
+    plugins: {
+      "postcss-nested": {},
+      "postcss-custom-media": {},
+    },
+  },
+  colorMode: {
+    preference: "dark",
+  },
 });
