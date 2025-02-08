f968fdab0cdd814754de38bca25c58434f5826a4
Matheus Almeida <tthheusalmeida@gmail.com>
2025-02-08 13:47:03 -0300

    fix: touch action on cellphone enable loading page

--- layouts/greetings.vue
+++ layouts/greetings.vue
@@ -3,3 +3,19 @@
     <slot />
   </div>
 </template>
+
+<script setup lang="ts">
+onMounted(() => {
+  document.body.classList.add("no-scroll");
+});
+
+onUnmounted(() => {
+  document.body.classList.remove("no-scroll");
+});
+</script>
+
+<style lang="postcss">
+.no-scroll {
+  @apply overflow-hidden fixed w-full h-full touch-none;
+}
+</style>
