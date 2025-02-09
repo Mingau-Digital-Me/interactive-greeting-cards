8d482c0bd440e4017cc0fe47ca2163b7667f236e
Matheus Almeida <tthheusalmeida@gmail.com>
2025-02-09 13:26:07 -0300

    chore: update logo icon and favicon

--- components/app/AppLogo.vue
+++ components/app/AppLogo.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="flex justify-center items-center">
-    <NuxtImg :height="40" src="./components/app/Logo/hardy-greetings.png" />
-  </div>
+  <UButton
+    :padded="false"
+    color="gray"
+    variant="link"
+    class="flex justify-center items-center"
+    @click="navigateToLandingPage"
+  >
+    <NuxtImg :height="40" src="./logo-white.png" />
+  </UButton>
 </template>
+
+<script setup lang="ts">
+async function navigateToLandingPage() {
+  await navigateTo({
+    path: "/",
+  });
+}
+</script>
